d607c758b31bb4511b2ef3273a659f7b35c6291d
Irfan Ahmed <38943111+iffi396@users.noreply.github.com>
2018-09-22 16:52:11 +0500

    Update java.js

--- java.js
+++ java.js
@@ -1,8 +1,8 @@
 
 function repo() {
-	window.location.href = "https://github.com/"+un+"/"+repo;
 	let un = document.querySelector(".un").value;
 	let repo = document.querySelector(".repo").value;
+	window.location.href = "https://github.com/"+un+"/"+repo;
 }
 function master() {
 	let un = document.querySelector(".un").value;
